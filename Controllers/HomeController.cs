4424e0d5784df81685e7b826b515a0aac1a0f0ac
Allan Oviedo <allan5kzx1@hotmail.com>
2021-04-19 20:37:36 -0600

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Montres.DAO;
+using Montres.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Montres.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+       
+        public JsonResult Permisos(string user, string contra)
+        {
+
+            LoginDAO dao = new LoginDAO();
+
+            int hola= dao.consultamenu(new Usuario(user, contra));
+           
+
+            return Json(hola, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Montres.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Montres
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+    }
+}
